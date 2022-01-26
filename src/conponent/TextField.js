7c7d3968ed7d2d61713bd1106d21f69daaf70293
dongguk <leedongguk@kakao.com>
2022-01-27 01:33:22 +0900

    useEffect 경로 변경

--- src/conponent/TextField.js
+++ src/conponent/TextField.js
@@ -1,5 +1,4 @@
-import React, { useRef, useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.input`
